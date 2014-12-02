7c314c7e81cd98388a8c89402c6457185f798a9d
rixhack <rafaelferrer@gmx.es>
2014-12-02 14:23:31 +0100

    Not capturing own pieces,knight and rook move constraints.

--- sandbox.c
+++ sandbox.c
@@ -14,41 +14,41 @@ int main(){
     
     nuevaPartida(pTablero);
     
-    setPieza(&tb1,0,"T",0,pTablero);
-    setPieza(&tb2,7,"T",0,pTablero);
-    setPieza(&cb1,1,"C",0,pTablero);
-    setPieza(&cb2,6,"C",0,pTablero);
-    setPieza(&ab1,2,"A",0,pTablero);
-    setPieza(&ab2,5,"A",0,pTablero);
-    setPieza(&db,3,"D",0,pTablero);
-    setPieza(&rb,4,"R",0,pTablero);
+    setPieza(&tb1,0,'T',0,pTablero);
+    setPieza(&tb2,7,'T',0,pTablero);
+    setPieza(&cb1,1,'C',0,pTablero);
+    setPieza(&cb2,6,'C',0,pTablero);
+    setPieza(&ab1,2,'A',0,pTablero);
+    setPieza(&ab2,5,'A',0,pTablero);
+    setPieza(&db,3,'D',0,pTablero);
+    setPieza(&rb,4,'R',0,pTablero);
 
-    setPieza(&pb1,8,"P",0,pTablero);
-    setPieza(&pb2,9,"P",0,pTablero);
-    setPieza(&pb3,10,"P",0,pTablero);
-    setPieza(&pb4,11,"P",0,pTablero);
-    setPieza(&pb5,12,"P",0,pTablero);
-    setPieza(&pb6,13,"P",0,pTablero);
-    setPieza(&pb7,14,"P",0,pTablero);
-    setPieza(&pb8,15,"P",0,pTablero);
+    setPieza(&pb1,8,'P',0,pTablero);
+    setPieza(&pb2,9,'P',0,pTablero);
+    setPieza(&pb3,10,'P',0,pTablero);
+    setPieza(&pb4,11,'P',0,pTablero);
+    setPieza(&pb5,12,'P',0,pTablero);
+    setPieza(&pb6,13,'P',0,pTablero);
+    setPieza(&pb7,14,'P',0,pTablero);
+    //setPieza(&pb8,15,'P',0,pTablero);
 
-    setPieza(&tn1,56,"t",1,pTablero);
-    setPieza(&tn2,63,"t",1,pTablero);
-    setPieza(&cn1,57,"c",1,pTablero);
-    setPieza(&cn2,62,"c",1,pTablero);
-    setPieza(&an1,58,"a",1,pTablero);
-    setPieza(&an2,61,"a",1,pTablero);
-    setPieza(&dn,59,"d",1,pTablero);
-    setPieza(&rn,60,"r",1,pTablero);
+    setPieza(&tn1,56,'t',1,pTablero);
+    setPieza(&tn2,63,'t',1,pTablero);
+    setPieza(&cn1,57,'c',1,pTablero);
+    setPieza(&cn2,62,'c',1,pTablero);
+    setPieza(&an1,58,'a',1,pTablero);
+    setPieza(&an2,61,'a',1,pTablero);
+    setPieza(&dn,59,'d',1,pTablero);
+    setPieza(&rn,60,'r',1,pTablero);
     
-    setPieza(&pn1,48,"p",1,pTablero);
-    setPieza(&pn2,49,"p",1,pTablero);
-    setPieza(&pn3,50,"p",1,pTablero);
-    setPieza(&pn4,51,"p",1,pTablero);
-    setPieza(&pn5,52,"p",1,pTablero);
-    setPieza(&pn6,53,"p",1,pTablero);
-    setPieza(&pn7,54,"p",1,pTablero);
-    setPieza(&pn8,55,"p",1,pTablero);
+    setPieza(&pn1,48,'p',1,pTablero);
+    setPieza(&pn2,49,'p',1,pTablero);
+    setPieza(&pn3,50,'p',1,pTablero);
+    setPieza(&pn4,51,'p',1,pTablero);
+    setPieza(&pn5,52,'p',1,pTablero);
+    setPieza(&pn6,53,'p',1,pTablero);
+    setPieza(&pn7,54,'p',1,pTablero);
+    setPieza(&pn8,55,'p',1,pTablero);
     
     updateTablero(pTablero);
     partidaEnJuego=1;
@@ -66,7 +66,7 @@ void updateTablero(tablero* pTablero){
             if(pTablero[i][j]->ocupada==0)
                 printf(" - ");
             else{
-                printf(" %s ",pTablero[i][j]->pieza->tipo);
+                printf(" %c ",pTablero[i][j]->pieza->tipo);
             }
          }
     }
@@ -83,7 +83,7 @@ void nuevaPartida(tablero* t){
     }
 }
 
-void setPieza(pieza* ppieza, int posicion, cadena tipo, int color,tablero* pTablero){
+void setPieza(pieza* ppieza, int posicion, char tipo, int color,tablero* pTablero){
     ppieza->posicion=posicion;
     ppieza->tipo=tipo;
     ppieza->color=color;
@@ -130,6 +130,7 @@ int toPos(char c){
 }
 
 void mover(int i1,int j1,int i2,int j2,tablero* pTablero){
+    int val=0;
     if(pTablero[i1][j1]->ocupada==0){
         printf("No hay ninguna pieza en esa posición.\n");
         jugar(pTablero);
@@ -138,18 +139,25 @@ void mover(int i1,int j1,int i2,int j2,tablero* pTablero){
         printf("No puedes mover una pieza del otro bando.\n");
         jugar(pTablero);
     }
-    else{    
-        pTablero[i2][j2]->pieza=pTablero[i1][j1]->pieza;
-        pTablero[i2][j2]->ocupada=1;
-        pTablero[i1][j1]->ocupada=0;
-        switch(turno){
-            case 0:
-                turno=1;
-                break;
-            case 1:
-                turno=0;
-                break;
+    else{
+        val = comprobarPieza(pTablero[i1][j1]->pieza,i1,j1,i2,j2,pTablero);
+        if(val){    
+            pTablero[i2][j2]->pieza=pTablero[i1][j1]->pieza;
+            pTablero[i2][j2]->ocupada=1;
+            pTablero[i1][j1]->ocupada=0;
+            switch(turno){
+                case 0:
+                    turno=1;
+                    break;
+                case 1:
+                    turno=0;
+                    break;
+            }
         }
+        else{
+            printf("Movimiento incorrecto.\n");
+            jugar(pTablero);
+        } 
     }
     
 }
@@ -163,10 +171,88 @@ int comprobarEntrada(int j1, int j2, int i1, int i2){
     return res;
 }
 
+int comprobarPieza(pieza* ppieza,int i1,int j1,int i2, int j2,tablero* pTablero){
+    int res=0;
+    int cc=comprobarCaptura(i1,j1,i2,j2,pTablero);
+    if(cc){
+        switch(ppieza->tipo){
+            case 'T': res=comprobarTorre(i1,j1,i2,j2,pTablero);
+            break;
+            case 'c':
+            case 'C': res=comprobarCaballo(i1,j1,i2,j2,pTablero);
+            break;
+            /*case 'A': res=comprobarAlfil(i1,j1,i2,j2,pTablero);
+            break;
+            case: 'D': res=comprobarDama(i1,j1,i2,j2,pTablero);
+            break;
+            case: 'R': res=comprobarRey(i1,j1,i2,j2,pTablero);
+            break;
+            case 'P': res=comprobarPeon(i1,j1,i2,j2,pTablero);
+            break;*/
+        }
+    }
+    return res;
+}
+
+int comprobarCaptura(int i1,int j1,int i2,int j2,tablero* pTablero){
+    int res=1;
+    if(pTablero[i2][j2]->ocupada==1){
+        if(pTablero[i2][j2]->pieza->color==turno)
+            res=0;
+    }
+    return res;
+}
+
+int comprobarCaballo(int i1, int j1, int i2, int j2, tablero* pTablero){
+    int res=1;
+    if(i1==i2||j1==j2||abs(i1-j1)==abs(i2-j2)||(i1+j1)==(i2+j2)||abs(i2-i1)>2||abs(j2-j1)>2)
+        res=0;
+    return res;
+}
+
+int comprobarTorre(int i1,int j1,int i2,int j2,tablero* pTablero){
+    int i;    
+    int res=1;
+    if(i1!=i2&&j1!=j2)
+        res=0;
+    else if(j1<j2){
+        for(i=j1+1;i<j2;i++){
+            if(pTablero[i1][i]->ocupada==1){
+                res=0;
+                break;
+            }
+        }
+    }
+    else if(j1>j2){
+        for(i=j1-1;i>j2;i--){
+            if(pTablero[i1][i]->ocupada==1){
+                res=0;
+                break;         
+            }
+        }
+    }
+    else if(i1<i2){
+    for(i=i1+1;i<i2;i++){
+        if(pTablero[i][j1]->ocupada==1){
+            res=0;
+            break;
+            }
+        }
+    }
+    else if(i1>i2){
+        for(i=i1-1;i>i2;i--){
+            if(pTablero[i][j1]->ocupada==1){
+                res=0;
+                break;         
+            }
+        }
+    }
+    return res;
+}
+
 void cleanInput(){
     int c;
     while(c!='\n' && c != EOF){
         c=getchar();
     }
 }
-     
--- sandbox.h
+++ sandbox.h
@@ -2,7 +2,7 @@ typedef char* cadena;
 
 typedef struct{
     int posicion;
-    cadena tipo;
+    char tipo;
     int color;
 } pieza;
 typedef pieza* ppieza;
@@ -13,10 +13,17 @@ typedef struct{
 typedef casilla tablero[8][8];
 
 void nuevaPartida(tablero*);
-void setPieza(pieza*,int,cadena,int,tablero*);
+void setPieza(pieza*,int,char,int,tablero*);
 void updateTablero(tablero*);
 void jugar(tablero*);
 int toPos(char);
 void mover(int,int,int,int,tablero*);
 int comprobarEntrada(int,int,int,int);
+int comprobarPieza(pieza*,int,int,int,int,tablero*);
+int comprobarCaptura(int,int,int,int,tablero*);
+int comprobarTorre(int,int,int,int,tablero*);
+int comprobarCaballo(int,int,int,int,tablero*);
+/*int comprobarDama(int,int,int,int);
+int comprobarRey(int,int,int,int);
+int comprobarPeon(int,int,int,int);*/
 void cleanInput();
